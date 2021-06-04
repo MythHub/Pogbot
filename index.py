6ccd0164ed2db9753b3fc8162f33c362f3ccffd3
MythHub <85370347+MythHub@users.noreply.github.com>
2021-06-04 17:41:25 -0400

    Update index.py

--- index.py
+++ index.py
@@ -77,3 +77,41 @@ if __name__ == "__main__":
         print("Status: Login unsuccessful.")
         # If the login fails then reset the token.
         reset_token()
+        
+        # Import the modules
+        import sys
+        import random
+        
+        ans = True
+        
+        while ans:
+            question = raw_input("Ask the magic 8 ball a qeustion: (press enter to quit) ")
+            
+            answers = random.randint(1,8)
+            
+            if question == "":
+                sys.exit()
+                
+            elif answers == 1:
+                print "It is certain"
+                
+            elif answers == 2:
+                print "Outlook good"
+                
+            elif answers == 3:
+                print "You may rely on it"
+                
+            elif answers == 4: 
+                print "Ask again later"
+                
+            elif answers == 5:
+                print "Concentrate and ask again"
+                
+            elif answers == 6: 
+                print "Reply hazy, try again"
+                
+            elif answers == 7:
+                print "My reply is no"
+                
+            elif answers == 8:
+                print "My sources say no"
